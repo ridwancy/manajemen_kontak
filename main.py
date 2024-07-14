48ddbeed7cb3cdc1e57a90ce93bf0500cfb91b5e
ridwancy <cyaliandi@apps.ipb.ac.id>
2024-07-14 23:06:10 +0700

    Menggunakan file dan exception/error handling

--- main.py
+++ main.py
@@ -1,63 +1,75 @@
-"Program Manajemen Kontak"
+def membuka_kontak(path='kontak.txt'):
+    try:
+        with open(path, 'r') as file:
+            kontak = file.readlines()
+        return kontak
+    except FileNotFoundError:
+        return []
+
+def menyimpan_kontak(path='kontak.txt', isi=[]):
+    with open(path, 'w') as file:
+        file.writelines(isi)
 
 class Kontak:
-  def __init__(self):
-    self.kontak =[]
+    def __init__(self, path='kontak.txt'):
+        self.path = path
+        self.kontak = membuka_kontak(path)
 
-  def melihat_kontak(self):
-    print("\nDaftar Kontak")
-    if self.kontak:
-      for num, item in enumerate(self.kontak, start=1):
-        print(f'{num}. {item["nama"]} ({item["telepon"]}, {item["email"]})')
-    else:
-      print("Kontak kosong")
-      return 1
-      
-  def menambah_kontak(self):
-    print("Tambah Kontak")
-    nama = input("Masukkan nama kontak baru: ")
-    telepon = input("Masukkan nomor telepon kontak baru: ")
-    email = input("Masukkan email kontak baru: ")
-    kontak_baru = {'nama': nama, 'telepon': telepon, 'email': email}
-    self.kontak.append(kontak_baru)
-    print("Kontak berhasil ditambahkan")
-    
-  def menghapus_kontak(self):
-    if self.melihat_kontak() == 1:
-      return
-    else:
-      i_hapus = int(input("Pilih nomor kontak yang akan dihapus: "))
-      del self.kontak[i_hapus-1]
-      print("Kontak berhasil dihapus")
+    def melihat_kontak(self):
+        print("\nDaftar Kontak")
+        if self.kontak:
+            for num, item in enumerate(self.kontak, start=1):
+                print(f'{num}. ' + item.strip())
+        else:
+            print("Kontak kosong")
+            return 1
 
+    def menambah_kontak(self):
+        print("Tambah Kontak")
+        nama = input("Masukkan nama kontak baru: ")
+        telepon = input("Masukkan nomor telepon kontak baru: ")
+        email = input("Masukkan email kontak baru: ")
+        kontak_baru = f'{nama}, {telepon}, {email}' + '\n'
+        self.kontak.append(kontak_baru)
+        print("Kontak berhasil ditambahkan")
 
-# kontak1 = {'nama': 'Joko', 'telepon': '08123456789', 'email': 'joko@gmail.com'}
-# kontak2 = {'nama': 'Andi', 'telepon': '08987654321', 'email': 'andi@gmail.com'}
-# kontak = [kontak1, kontak2]
+    def menghapus_kontak(self):
+        if self.melihat_kontak() == 1:
+            return
+        else:
+            try:
+                i_hapus = int(input("Pilih nomor kontak yang akan dihapus: "))
+                del self.kontak[i_hapus-1]
+                print("Kontak berhasil dihapus")
+            except (ValueError, IndexError):
+                print("Kontak gagal dihapus. Input yang Anda masukkan salah")
+
+    def keluar_kontak(self):
+        menyimpan_kontak(self.path, self.kontak)
 
 kontak_kantor = Kontak()
-kontak_keluarga = Kontak()
 
 while True:
-  print("\nMenu Kontak")
-  print("1. Daftar Kontak")
-  print("2. Tambah Kontak")
-  print("3. Menghapus Kontak")
-  print("4. Keluar")
+    print("\nMenu Kontak")
+    print("1. Daftar Kontak")
+    print("2. Tambah Kontak")
+    print("3. Menghapus Kontak")
+    print("4. Keluar")
 
-  pilihan = input("Pilih menu: ")
+    pilihan = input("Pilih menu: ")
 
-  if pilihan == "1":
-    kontak_kantor.melihat_kontak()
-  
-  elif pilihan == "2":
-    kontak_kantor.menambah_kontak()
+    if pilihan == "1":
+        kontak_kantor.melihat_kontak()
     
-  elif pilihan == "3":
-    kontak_kantor.menghapus_kontak()
+    elif pilihan == "2":
+        kontak_kantor.menambah_kontak()
+        
+    elif pilihan == "3":
+        kontak_kantor.menghapus_kontak()
+        
+    elif pilihan == "4":
+        kontak_kantor.keluar_kontak()
+        break
     
-  elif pilihan == "4":
-    break
-  
-  else:
-    print("Anda memasukkan pilihan yang salah. Silahkan coba lagi.")
+    else:
+        print("Anda memasukkan pilihan yang salah. Silahkan coba lagi.")
